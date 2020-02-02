03d0c860d9abfec4c65d794f7f0b470191b31d56
Z-VV <44175275+Z-VV@users.noreply.github.com>
2020-02-02 19:38:46 +0000

    Update FURY2.py

--- FURY2.py
+++ FURY2.py
@@ -1,27 +1,24 @@
+
 import time
 import datetime as dt
 import pandas as pd
 import shutil
 import fxcmpy
 from playsound import  playsound
-from talib import EMA
+from socketIO_client import SocketIO
 
 def connect():
-    #Connects
-    
-    token = 'insert token here'  
-    con = fxcmpy.fxcmpy(access_token=token, log_level="error")
+
+    token ='786733bea669469874c5b4f45be1e1766ac9066b' # 'b7ff7cbe3d6735a548ba8f33a37c78d465f847b2'  # demo
+    con = fxcmpy.fxcmpy(access_token=token, log_level="error",server='real')
     print(con.get_accounts())
     return con,token
 
-# Originaly  FIX (Financial Information Exchange) API is designed to work with C++ mostly,beacause of the speed of data streaming
-# and orders(see high frequency trading).
-# C++ code is working simultaneously with this code.To take up yo 200 price changes a second,our code must first copy the C log.file 
-# in order to not interrupt the writing and then read the new file.
 
 def Run(con,token):
     symbol = 'GBP/USD'
     do=True
+    data_m1=None
     info = con.get_open_positions(kind=list)
     for x in range(0, len(info)):
         if info.iloc[x]['currency'] == symbol:
@@ -31,9 +28,19 @@ def Run(con,token):
             do = True
     data = pd.DataFrame(columns = ['time','bid','tick'])
     N=0
+    bid=0
+    h=0
+    candle_1_open=0
+    candle_1_close=0
+    candle_2_open=0
+    candle_2_close=0
+    candle_3_open=0
+    candle_3_close=0
 
     while True:
-            time1 = dt.datetime.now()
+        time1 = dt.datetime.now()
+        if time1.second!=h:
+            h = time1.second
             shutil.copy2('C:/Users\master\Desktop\\fix_example_x64 (1)\\fix_example\\fix_example\Logs\\' + 'FIX.4.4-MD_701299628_client1-FXCM.messages.current.log',\
                          'C:/Users\master\Desktop\\fix_example_x64 (1)\\fix_example\\fix_example\Logs\\' + 'new.log')
             f = open('C:/Users\master\Desktop\\fix_example_x64 (1)\\fix_example\\fix_example\Logs\\' + 'new.log','r')
@@ -42,21 +49,26 @@ def Run(con,token):
 
             num=len(lines)
             raw=lines[-1].split('270=')
-            
-            # Creating dataframe from the tick data avaliable from the log.file 
-            if len(raw)>4:
-                N1 = num - N
-                N = num
-                if N1!=0 and N1<150:
+
+            N1 = num - N
+            N = num
+            if N1!=0 and N1<150 and len(raw)>4:
+                for x in range(N1):
                     timenow = dt.datetime.now()
+                    raw = lines[-x]
                     bid_list=raw[1].split('')
                     bid=float(bid_list[0])
-                    dic={'time':timenow,'bid':bid,'tick':N1}
+                    dic={'time':timenow,'bid':bid,'tick':1}
                     data=data.append(dic,ignore_index=True,sort=False)
                     print(data.tail(1))
-                    
-            # Going throw the dataframe every second and checking for signals as at FuryBacktest.py . 
-            if len(data)>121:
+            if N1==0 or len(raw)<=4:
+                timenow = dt.datetime.now()
+                dic = {'time': timenow, 'bid': bid, 'tick': 0}
+                data = data.append(dic, ignore_index=True, sort=False)
+                print(data.tail(1))
+
+
+            if len(data)>185:
                 two_minute = data.iloc[-120:-10]
                 two_minute=two_minute.reset_index(drop=True)
                 tick_sum = 0
@@ -78,27 +90,59 @@ def Run(con,token):
                 print('----------------------------')
 
                 if (actual_ten_seconds_sum - average_ticks_for_ten_seconds) > average_ticks_for_ten_seconds*0.8 and do:
-                    if (data['bid'][len(data)-1] - maximum) >= (maximum - minimum)*0.8 :
-                        print((maximum - minimum) * 10000)
-                        opentrade = con.open_trade(symbol=symbol, is_buy=True, amount=5,
-                                                   time_in_force='GTC',
-                                                   order_type='AtMarket', is_in_pips=True, limit=3,
-                                                   stop=-2)
-                        do=False
-                        playsound('warp.mp3')
+                    #print('downtrend    '+str(downtrend))
+                    #print('uptrend    ' + str(uptrend))
 
-                    if (minimum - data['bid'][len(data)-1]) >= (maximum - minimum)*0.8 :
-                        print((maximum - minimum) * 10000)
-                        opentrade = con.open_trade(symbol=symbol, is_buy=False, amount=5,
-                                                   time_in_force='GTC',
-                                                   order_type='AtMarket', is_in_pips=True, limit=3,
-                                                   stop=-2)
-                        do=False
-                        playsound('warp.mp3')
+                    if candle_1_open<candle_1_close and candle_2_open<candle_2_close and candle_3_open<candle_3_close :
 
-            if time1.second==10:
+                        if (data['bid'][len(data)-1] - maximum) >= (maximum - minimum)*0.8 :
+                            print((maximum - minimum) * 10000)
+                            opentrade = con.open_trade(symbol=symbol, is_buy=True, amount=1,
+                                                       time_in_force='GTC',
+                                                       order_type='AtMarket', is_in_pips=True, limit=5,
+                                                       stop=-5)
+                            do=False
+                            playsound('warp.mp3')
+
+                    if candle_1_open > candle_1_close and candle_2_open > candle_2_close and candle_3_open > candle_3_close:
+
+                        if (minimum - data['bid'][len(data)-1]) >= (maximum - minimum)*0.8 :
+                            print((maximum - minimum) * 10000)
+                            opentrade = con.open_trade(symbol=symbol, is_buy=False, amount=1,
+                                                       time_in_force='GTC',
+                                                       order_type='AtMarket', is_in_pips=True, limit=5,
+                                                       stop=-5)
+                            do=False
+                            playsound('warp.mp3')
+
+            if time1.second==1 and len(data)>185:
                 connected=con.is_connected()
                 print(connected)
+                candle_1_close = data['bid'][len(data)-2]
+                candle_1_open = data['bid'][len(data)-62]
+                candle_2_close = data['bid'][len(data)-62]
+                candle_2_open = data['bid'][len(data)-122]
+                candle_3_close = data['bid'][len(data)-122]
+                candle_3_open = data['bid'][len(data)-182]
+                print(data['time'][len(data)-2])
+                print(data['time'][len(data)-62])
+                print(data['time'][len(data)-62])
+                print(data['time'][len(data)-122])
+                print(data['time'][len(data)-122])
+                print(data['time'][len(data)-182])
+
+                if candle_1_open<candle_1_close:
+                    print('candle 1   +')
+                else:
+                    print('candle 1   -')
+                if candle_2_open<candle_2_close:
+                    print('candle 2   +')
+                else:
+                    print('candle 2   -')
+                if candle_3_open<candle_3_close:
+                    print('candle 3   +')
+                else:
+                    print('candle 3   -')
 
                 if connected==False:
                     con = fxcmpy.fxcmpy(access_token=token, log_level="error")
@@ -113,11 +157,18 @@ def Run(con,token):
                             break
                         else:
                             do=True
-
             time2 = dt.datetime.now()
-            print(time2 - time1)
-            time.sleep(1)
+            print(((time2 - time1).microseconds)/1000000)
 
+        time.sleep(0.1)
 
+print(fxcmpy.__version__)
 con,token=connect()
 Run(con,token)
+while True:
+    try:
+        con,token=connect()
+        Run(con,token)
+    except Exception as e:
+        print(e)
+        time.sleep(120)
